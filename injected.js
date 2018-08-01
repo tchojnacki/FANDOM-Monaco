5f58032634cc8336edc1cdfec3d7b59fba8d71d6
T. Chojnacki <tomekch6@gmail.com>
2018-08-01 18:33:32 +0200

    Enable on MediaWiki pages

--- injected.js
+++ injected.js
@@ -2,9 +2,29 @@
   if (window.$('#FandomMonaco').length !== 0) {
     return
   }
-  const config = window.mw.config.get(['wgPageName', 'wgScriptPath', 'wgUserName', 'wgNamespaceNumber'])
-  if (config.wgPageName.match(new RegExp(`:${config.wgUserName}/.*\\.(js|css)$`)) && config.wgNamespaceNumber === 2) {
-    const lang = config.wgPageName.match(/\.(css|js)$/)[1] === 'js' ? 'javascript' : 'css'
+  const config = window.mw.config.get(['wgPageName', 'wgScriptPath', 'wgUserName', 'wgNamespaceNumber', 'wgUserGroups'])
+  let enabled = false
+  let lang = 'plain'
+  if (config.wgNamespaceNumber === 2) {
+    if (config.wgPageName.match(new RegExp(`:${config.wgUserName}/.*\\.js$`))) {
+      enabled = true
+      lang = 'javascript'
+    } else if (config.wgPageName.match(new RegExp(`:${config.wgUserName}/.*\\.css$`))) {
+      enabled = true
+      lang = 'css'
+    }
+  } else if (config.wgNamespaceNumber === 8) {
+    if (config.wgUserGroups.includes('sysop')) {
+      enabled = true
+    }
+    if (config.wgPageName.endsWith('.js')) {
+      lang = 'javascript'
+    } else if (config.wgPageName.endsWith('.css')) {
+      lang = 'css'
+    }
+  }
+
+  if (enabled) {
     window.$('.page-header__contribution-buttons .wds-list').append(
       window.$('<li>').append(
         window.$('<a>', {
