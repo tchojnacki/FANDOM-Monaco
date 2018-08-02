2da473bef85e5b1cd527d336c7904cd5be683967
T. Chojnacki <tomekch6@gmail.com>
2018-08-02 17:52:07 +0200

    Edit without preserved tab

--- background.js
+++ background.js
@@ -13,22 +13,47 @@ browser.runtime.onMessage.addListener(async (request) => {
       window.data = request.data
       window.editor = await browser.windows.create({
         type: 'popup',
-        url: browser.extension.getURL('editor.html'),
-        width: 800,
-        height: 600
+        url: browser.extension.getURL('editor.html')
       })
       break
     case 'make_edit':
       browser.windows.remove(window.editor.id)
-      browser.windows.getAll({populate: true, windowTypes: ['normal']}, (windows) => {
-        browser.tabs.sendMessage(windows[0].tabs.find((tab) => window.data.url.startsWith(tab.url)).id, {
-          type: 'make_edit',
-          data: {
-            text: request.data.text,
-            summary: request.data.summary,
-            title: window.data.title
+      browser.windows.getAll({populate: true, windowTypes: ['normal']}, (wins) => {
+        const foundTabs = []
+        wins.forEach((win) => {
+          const tab = win.tabs.find((tab) => window.data.url.startsWith(tab.url))
+          if (tab) {
+            foundTabs.push(tab.id)
           }
         })
+        if (foundTabs.length > 0) {
+          browser.tabs.sendMessage(foundTabs[0], {
+            type: 'make_edit',
+            data: {
+              text: request.data.text,
+              summary: request.data.summary,
+              title: window.data.title
+            }
+          })
+        } else { // Tab got closed
+          browser.tabs.create({
+            url: window.data.url
+          }, async (newTab) => {
+            browser.tabs.onUpdated.addListener(function listener (tabId, info) {
+              if (info.status === 'complete' && tabId === newTab.id) {
+                browser.tabs.onUpdated.removeListener(listener)
+                browser.tabs.sendMessage(newTab.id, {
+                  type: 'make_edit',
+                  data: {
+                    text: request.data.text,
+                    summary: request.data.summary,
+                    title: window.data.title
+                  }
+                })
+              }
+            })
+          })
+        }
       })
       break
     case 'close_editor':
