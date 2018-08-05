cc2a4a01e50f550442f9db0b98b7fcf3f74ec0c7
T. Chojnacki <tomekch6@gmail.com>
2018-08-05 15:08:06 +0200

    Start editor.js rewrite

--- editor.js
+++ editor.js
@@ -1,20 +1,96 @@
 /* global monaco, browser, JSHINT */
-require(['vs/editor/editor.main'], async () => {
-  async function getBackgroundData () {
-    return (await browser.runtime.getBackgroundPage()).data
+
+class FMEditor {
+  constructor () {
+    this.editorVisible = true
+    FMEditor.instance = this
   }
 
-  async function getPageContent () {
-    const data = await getBackgroundData()
-    const response = await window.fetch(`${data.api}/api.php?action=query&titles=${data.title}&prop=revisions&rvprop=content&format=json&cb=${Math.floor(new Date().getTime() / 1000)}`, {
-      cache: 'no-store' /* The CacheBuster might be heavy on the servers but we NEED the most recent version if we want to edit. */
+  async init () {
+    await this.getBackgroundData() // Now this.bgData exists
+    this.setDocumentUp()
+    this.previousContent = await this.getPageContent()
+    await this.setEditorsUp() // Now this.editor exists (and sometimes this.diffEditor)
+    this.hideSpinner()
+    this.createHandlers()
+    return this
+  }
+
+  async getBackgroundData () {
+    this.bgData = (await browser.runtime.getBackgroundPage()).data
+  }
+
+  async getPageContent () {
+    const response = await window.fetch(`${this.bgData.api}/api.php?action=query&titles=${this.bgData.title}&prop=revisions&rvprop=content&format=json&cb=${Math.floor(new Date().getTime() / 1000)}`, {
+      cache: 'no-store' // The CacheBuster might be heavy on the servers but we NEED the most recent version if we want to edit.
     })
     const json = await response.json()
     const content = json.query.pages[Object.keys(json.query.pages)[0]].revisions ? json.query.pages[Object.keys(json.query.pages)[0]].revisions[0]['*'] : ''
     return content
   }
 
-  function hideSpinner () {
+  setDocumentUp () {
+    document.getElementById('pagename').textContent = this.bgData.title
+    document.getElementById('pagename').setAttribute('href', this.bgData.url)
+    if (this.bgData.mode !== 'inspect') {
+      document.getElementById('diff').style.display = 'block'
+      document.getElementById('summary').style.display = 'inline-block'
+      document.getElementById('diff-container').style.display = 'block'
+    }
+  }
+
+  async setEditorsUp () {
+    if (this.bgData.lang === 'javascript') {
+      monaco.languages.typescript.javascriptDefaults.addExtraLib(
+        (await (await window.fetch('./lib.d.ts')).text()),
+        'lib.d.ts'
+      )
+    }
+    this.editor = monaco.editor.create(document.getElementById('editor-container'), {
+      value: this.previousContent,
+      language: this.bgData.lang,
+      theme: 'vs-dark',
+      readOnly: true
+    })
+    if (this.bgData.lang === 'javascript' && this.bgData.mode !== 'inspect') {
+      this.lint()
+      this.diffEditor = monaco.editor.createDiffEditor(document.getElementById('diff-container'))
+    }
+    if (this.bgData.mode !== 'inspect') {
+      this.editor.updateOptions({ readOnly: false })
+    }
+  }
+
+  lint () {
+    JSHINT.jshint(this.editor.getValue(), {
+      esversion: 5,
+      curly: true,
+      eqeqeq: true,
+      freeze: true,
+      futurehostile: true,
+      latedef: true,
+      nocomma: true,
+      nonbsp: true,
+      shadow: false,
+      strict: 'implied',
+      '-W117': true, // No undef - Wikia's got a lot of weird global variables
+      unused: true,
+      asi: true,
+      eqnull: true
+    })
+    monaco.editor.setModelMarkers(this.editor.getModel(), 'jshint', (JSHINT.jshint.data().errors || []).map((e) => {
+      return {
+        startLineNumber: e.line,
+        startColumn: e.character,
+        endLineNumber: e.line,
+        endColumn: e.character,
+        message: e.reason,
+        severity: e.code.startsWith('E') ? monaco.Severity.Error : monaco.Severity.Warning
+      }
+    }))
+  }
+
+  hideSpinner () {
     document.getElementsByClassName('overlay')[0].classList.add('invisible')
     setTimeout(() => {
       document.getElementsByClassName('spinner')[0].classList.add('hidden')
@@ -22,7 +98,7 @@ require(['vs/editor/editor.main'], async () => {
     }, 500)
   }
 
-  function showDialog (title, content, actions) {
+  showDialog (title, content, actions) {
     return new Promise((resolve) => {
       const overlay = document.getElementsByClassName('overlay')[0]
       const dialog = document.getElementsByClassName('dialog')[0]
@@ -45,146 +121,86 @@ require(['vs/editor/editor.main'], async () => {
     })
   }
 
-  window.editorVisible = true
-  const { title, url, lang, mode } = await getBackgroundData()
-  document.title = title
-  document.getElementById('pagename').textContent = title
-  document.getElementById('pagename').setAttribute('href', url)
-  if (mode !== 'inspect') {
-    document.getElementById('diff').style.display = 'block'
-    document.getElementById('summary').style.display = 'inline-block'
-    document.getElementById('diff-container').style.display = 'block'
-  }
-  window.previousContent = await getPageContent()
-
-  if (lang === 'javascript') {
-    monaco.languages.typescript.javascriptDefaults.addExtraLib(
-      (await (await window.fetch('./lib.d.ts')).text()),
-      'lib.d.ts'
-    )
-  }
-
-  window.editor = monaco.editor.create(document.getElementById('editor-container'), {
-    value: window.previousContent,
-    language: lang,
-    theme: 'vs-dark',
-    readOnly: true
-  })
-
-  if (lang === 'javascript') {
-    window.jshintConfig = {
-      esversion: 5,
-      curly: true,
-      eqeqeq: true,
-      freeze: true,
-      futurehostile: true,
-      latedef: true,
-      nocomma: true,
-      nonbsp: true,
-      shadow: false,
-      strict: 'implied',
-      '-W117': true, /* No undef - Wikia's got a lot of weird global variables */
-      unused: true,
-      asi: true,
-      eqnull: true
-    }
-    window.jshintMap = (e) => {
-      return {
-        startLineNumber: e.line,
-        startColumn: e.character,
-        endLineNumber: e.line,
-        endColumn: e.character,
-        message: e.reason,
-        severity: e.code.startsWith('E') ? monaco.Severity.Error : monaco.Severity.Warning
-      }
-    }
-    JSHINT.jshint(window.editor.getValue(), window.jshintConfig)
-    monaco.editor.setModelMarkers(window.editor.getModel(), 'jshint', (JSHINT.jshint.data().errors || []).map(window.jshintMap))
-  }
-
-  hideSpinner()
-
-  if (mode !== 'inspect') {
-    window.editor.model.onDidChangeContent((event) => {
-      if (window.editor.getValue() !== window.previousContent) {
-        document.getElementById('publish').textContent = window.previousContent === '' ? 'Publikuj' : 'Zapisz'
-      } else {
-        document.getElementById('publish').textContent = 'Zamknij'
-      }
-      if (lang === 'javascript') {
-        JSHINT.jshint(window.editor.getValue(), window.jshintConfig)
-        monaco.editor.setModelMarkers(window.editor.getModel(), 'jshint', (JSHINT.jshint.data().errors || []).map(window.jshintMap))
+  createHandlers () {
+    window.addEventListener('resize', () => {
+      this.editor.layout()
+      if (this.bgData.mode !== 'inspect') {
+        this.diffEditor.layout()
       }
     })
 
-    window.diffEditor = monaco.editor.createDiffEditor(document.getElementById('diff-container'))
+    if (this.bgData.mode !== 'inspect') {
+      this.editor.model.onDidChangeContent((event) => {
+        if (this.editor.getValue() !== this.previousContent) {
+          document.getElementById('publish').textContent = this.previousContent === '' ? 'Publikuj' : 'Zapisz'
+        } else {
+          document.getElementById('publish').textContent = 'Zamknij'
+        }
+        if (this.bgData.lang === 'javascript') {
+          this.lint()
+        }
+      })
 
-    window.editor.updateOptions({ readOnly: false })
-  }
+      document.getElementById('diff').addEventListener('click', async () => {
+        if (this.editorVisible) {
+          this.editorVisible = false
+          document.getElementById('editor-container').style.setProperty('flex', '0')
+          document.getElementById('diff-container').style.setProperty('flex', '1')
+          document.getElementById('diff').textContent = 'Edytuj'
 
-  window.addEventListener('resize', () => {
-    window.editor.layout()
-    if (mode !== 'inspect') {
-      window.diffEditor.layout()
+          const originalModel = monaco.editor.createModel(this.previousContent, `text/${this.bgData.lang}`)
+          const modifiedModel = monaco.editor.createModel(this.editor.getValue(), `text/${this.bgData.lang}`)
+
+          this.diffEditor.setModel({
+            original: originalModel,
+            modified: modifiedModel
+          })
+        } else {
+          this.editorVisible = true
+          document.getElementById('editor-container').style.setProperty('flex', '1')
+          document.getElementById('diff-container').style.setProperty('flex', '0')
+          document.getElementById('diff').textContent = 'Różnica'
+        }
+        this.editor.layout()
+        this.diffEditor.layout()
+      })
     }
-  })
 
-  if (mode !== 'inspect') {
-    document.getElementById('diff').addEventListener('click', async () => {
-      if (window.editorVisible) {
-        window.editorVisible = false
-        document.getElementById('editor-container').style.setProperty('flex', '0')
-        document.getElementById('diff-container').style.setProperty('flex', '1')
-        document.getElementById('diff').textContent = 'Edytuj'
-
-        const originalModel = monaco.editor.createModel(window.previousContent, `text/${lang}`)
-        const modifiedModel = monaco.editor.createModel(window.editor.getValue(), `text/${lang}`)
-
-        window.diffEditor.setModel({
-          original: originalModel,
-          modified: modifiedModel
+    document.getElementById('publish').addEventListener('click', async () => {
+      if (this.editor.getValue() !== this.previousContent && this.bgData.mode !== 'inspect') {
+        if (this.bgData.mode === 'editwarning') {
+          const dialog = await this.showDialog(
+            'Nie jesteś administratorem',
+            'Edytujesz ten plik jedynie na mocy swoich uprawnień globalnych. Upewnij się, że masz zgodę lokalnej społeczności na dokonanie zmian.',
+            [{
+              action: 'CANCEL',
+              text: 'Anuluj'
+            }, {
+              action: 'PUBLISH',
+              text: 'Publikuj'
+            }]
+          )
+          if (dialog !== 'PUBLISH') {
+            return
+          }
+        }
+        // TODO: Check for edit conflicts
+        browser.runtime.sendMessage({
+          type: 'MAKE_EDIT:E->B',
+          data: {
+            text: this.editor.getValue(),
+            summary: document.getElementById('summary').value
+          }
         })
       } else {
-        window.editorVisible = true
-        document.getElementById('editor-container').style.setProperty('flex', '1')
-        document.getElementById('diff-container').style.setProperty('flex', '0')
-        document.getElementById('diff').textContent = 'Różnica'
+        browser.runtime.sendMessage({
+          type: 'CLOSE_EDITOR:E->B'
+        })
       }
-      window.editor.layout()
-      window.diffEditor.layout()
     })
   }
+}
 
-  document.getElementById('publish').addEventListener('click', async () => {
-    if (window.editor.getValue() !== window.previousContent && mode !== 'inspect') {
-      if (mode === 'editwarning') {
-        const dialog = await showDialog(
-          'Nie jesteś administratorem',
-          'Edytujesz ten plik jedynie na mocy swoich uprawnień globalnych. Upewnij się, że masz zgodę lokalnej społeczności na dokonanie zmian.',
-          [{
-            action: 'CANCEL',
-            text: 'Anuluj'
-          }, {
-            action: 'PUBLISH',
-            text: 'Publikuj'
-          }]
-        )
-        if (dialog !== 'PUBLISH') {
-          return
-        }
-      }
-      // TODO: Check for edit conflicts
-      browser.runtime.sendMessage({
-        type: 'MAKE_EDIT:E->B',
-        data: {
-          text: window.editor.getValue(),
-          summary: document.getElementById('summary').value
-        }
-      })
-    } else {
-      browser.runtime.sendMessage({
-        type: 'CLOSE_EDITOR:E->B'
-      })
-    }
-  })
+require(['vs/editor/editor.main'], async () => {
+  await new FMEditor().init()
 })
