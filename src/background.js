e714911c7985cc98ad86ce6ba013a892f58d0a63
T. Chojnacki <tomekch6@gmail.com>
2018-11-25 15:53:22 +0100

    Add some comments

--- src/background.js
+++ src/background.js
@@ -4,14 +4,14 @@ window.i18nReady = false
 
 browser.runtime.onMessage.addListener(async (request, sender) => {
   switch (request.type) {
-    case 'OPEN_EDITOR:C->B':
+    case 'OPEN_EDITOR:C->B': // Message from content.js
       if (window.editor) {
         try {
           const win = await browser.windows.get(window.editor.id)
           if (win) {
             // Editor is already open
             browser.tabs.sendMessage(sender.tab.id, {
-              type: 'DISPLAY_BANNER:B->C',
+              type: 'DISPLAY_BANNER:B->C', // Message to content.js
               data: {
                 text: window.msg('ALREADY_OPEN', request.data.i18n),
                 timeout: 2000
@@ -22,12 +22,12 @@ browser.runtime.onMessage.addListener(async (request, sender) => {
         } catch (e) {}
       }
       window.data = request.data
-      window.editor = await browser.windows.create({
+      window.editor = await browser.windows.create({ // Open the editor
         type: 'popup',
         url: browser.extension.getURL('src/editor.html')
       })
       break
-    case 'MAKE_EDIT:E->B':
+    case 'MAKE_EDIT:E->B': // Message from editor.js
       browser.windows.remove(window.editor.id)
       browser.windows.getAll({populate: true, windowTypes: ['normal']}).then((wins) => { // Find all non-popup windows
         const foundTabs = []
@@ -37,9 +37,9 @@ browser.runtime.onMessage.addListener(async (request, sender) => {
             foundTabs.push(tab.id)
           }
         })
-        if (foundTabs.length > 0) {
+        if (foundTabs.length > 0) { // Tab found
           browser.tabs.sendMessage(foundTabs[0], { // Only send the message to one content script
-            type: 'MAKE_EDIT:B->C',
+            type: 'MAKE_EDIT:B->C', // Message to content.js
             data: {
               ...request.data,
               title: window.data.title,
@@ -47,7 +47,7 @@ browser.runtime.onMessage.addListener(async (request, sender) => {
               networkerror: window.msg('NETWORK_ERROR', window.data.i18n)
             }
           })
-          browser.tabs.onUpdated.addListener(function listener (tabId, info) {
+          browser.tabs.onUpdated.addListener(function listener (tabId, info) { // After editing and reloading
             if (info.status === 'complete' && tabId === foundTabs[0]) { // Wait until the tab is loaded
               browser.tabs.onUpdated.removeListener(listener)
               browser.tabs.sendMessage(foundTabs[0], {
@@ -67,20 +67,20 @@ browser.runtime.onMessage.addListener(async (request, sender) => {
             let alreadyOpened = false
             browser.tabs.onUpdated.addListener(function listener (tabId, info) {
               if (info.status === 'complete' && tabId === newTab.id) { // Wait until the tab is loaded
-                if (alreadyOpened) {
+                if (alreadyOpened) { // Second load
                   browser.tabs.onUpdated.removeListener(listener)
                   browser.tabs.sendMessage(newTab.id, {
-                    type: 'DISPLAY_BANNER:B->C',
+                    type: 'DISPLAY_BANNER:B->C', // Message to content.js
                     data: {
                       text: window.msg('EDIT_SUCCESS', window.data.i18n),
                       type: 'confirm',
                       timeout: 2000
                     }
                   })
-                } else {
+                } else { // First load
                   alreadyOpened = true
                   browser.tabs.sendMessage(newTab.id, {
-                    type: 'MAKE_EDIT:B->C',
+                    type: 'MAKE_EDIT:B->C', // Message to content.js
                     data: {
                       ...request.data,
                       title: window.data.title,
@@ -95,13 +95,13 @@ browser.runtime.onMessage.addListener(async (request, sender) => {
         }
       })
       break
-    case 'CLOSE_EDITOR:E->B':
+    case 'CLOSE_EDITOR:E->B': // Message from editor.js
       browser.windows.remove(window.editor.id)
       break
   }
 })
 
-window.msg = (msg, lang) => {
+window.msg = (msg, lang) => { // Get i18n message
   if (lang && msg && window.i18nReady && window.i18n[lang] && window.i18n[lang][msg]) {
     return window.i18n[lang][msg]
   }
@@ -116,6 +116,7 @@ window.msg = (msg, lang) => {
 
 async function getTranslations () {
   try {
+    // Translations should only load once so the servers aren't spammed
     const response = await window.fetch('https://dev.wikia.com/wiki/MediaWiki:Custom-FANDOM-Monaco/i18n.json?action=raw')
     const text = await response.text()
 
